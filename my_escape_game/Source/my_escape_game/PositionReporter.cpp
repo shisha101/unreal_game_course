c2c42151df4a75210d9b3ee41b8e0e1116348ba4
Shehab Abdelgawad <shehabxshehab@hotmail.com>
2018-05-14 23:29:23 +0200

    added position_reporter roll pitch yaw

--- my_escape_game/Source/my_escape_game/PositionReporter.cpp
+++ my_escape_game/Source/my_escape_game/PositionReporter.cpp
@@ -9,7 +9,7 @@ UPositionReporter::UPositionReporter()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bCanEverTick = true;
 
     // ...
 }
@@ -20,9 +20,12 @@ void UPositionReporter::BeginPlay()
 {
     Super::BeginPlay();
     const auto object_location = GetOwner()->GetActorLocation();
-    UE_LOG(LogTemp,Warning,TEXT("%s is in the BeginPlay function x=%f, y=%f, z=%f"),
+    const auto rotation_euler_deg = GetOwner()->GetActorRotation().Euler();
+
+    UE_LOG(LogTemp,Warning,TEXT("%s is in the BeginPlay function Postion: x=%f, y=%f, z=%f. Rotation (Euler): r=%f, p=%f, y=%f ."),
            *GetOwner()->GetHumanReadableName(),
-           object_location.X, object_location.Y, object_location.Z);
+           object_location.X, object_location.Y, object_location.Z,
+           rotation_euler_deg.X, rotation_euler_deg.Y, rotation_euler_deg.Z);
 }
 
 
