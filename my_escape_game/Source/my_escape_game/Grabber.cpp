080602f54431b1d0b54c58cc0063edb9dbd51c08
Shehab Abdelgawad <shehabxshehab@hotmail.com>
2018-05-23 01:44:47 +0200

    added line trace log for physics objects

--- my_escape_game/Source/my_escape_game/Grabber.cpp
+++ my_escape_game/Source/my_escape_game/Grabber.cpp
@@ -29,8 +29,15 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
   FVector view_location;
   FRotator view_orientation;
   GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(view_location, view_orientation);
+  const auto ray_end_point = view_location + view_orientation.Vector() * player_grabbing_reach_;
   //    GetOwner()->GetActorEyesViewPoint(view_location,view_orientation);
 
-  DrawDebugLine(GetWorld(), view_location, view_location + view_orientation.Vector() * player_grabbing_reach_,
-                FColor(255, 0, 0), false, -1.f, 0, 1);
+  DrawDebugLine(GetWorld(), view_location, ray_end_point, FColor(255, 0, 0), false, -1.f, 0, 1);
+
+  FHitResult object_hit;
+  const FCollisionQueryParams collision_query_params(FName(""), false, GetOwner());
+  if (GetWorld()->LineTraceSingleByObjectType(object_hit, view_location, ray_end_point,
+                                              FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+                                              FCollisionQueryParams(FName(""), false, GetOwner())))
+    UE_LOG(LogTemp, Warning, TEXT("can grab %s"), *object_hit.GetActor()->GetName());
 }
